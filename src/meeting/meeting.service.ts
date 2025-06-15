01817ea9c3d945c6cef84ee8000f3e14d5a72f70
fazrilrama <fazrilramadhan2000@gmail.com>
2025-06-15 15:25:13 +0700

    fix

--- src/meeting/meeting.service.ts
+++ src/meeting/meeting.service.ts
@@ -44,40 +44,86 @@ export class MeetingService {
           conditions.push('meeting_code = ?');
           params.push(meeting_code);
         }
-      
-        if (user_id) {
-          conditions.push('created_by = ?');
-          params.push(user_id);
+
+        if(user_id) {
+            conditions.push('meetings.created_by = ?');
+            params.push(user_id);
         }
       
         if (conditions.length > 0) {
           sql += ' WHERE ' + conditions.join(' AND ');
         }
-      
+
         const rows: any = await query(sql, params);
 
-        const meeting = rows[0];
 
-        if (meeting_code && meeting) {
-            const sql_record = 'SELECT id, record_name, created_at FROM meeting_record WHERE meeting_id = ?';
-            const sql_participant = `
-                SELECT meeting_participant.id, users.fullname, users.email, status_accept.name as status_name  FROM meeting_participant 
-                    LEFT JOIN users ON users.id = meeting_participant.user_id
-                    LEFT JOIN status_accept ON status_accept.id = meeting_participant.status_accept
-                WHERE meeting_id = ?
+        let meet_participant = `
+                SELECT meetings.*, users.fullname FROM meeting_participant
+                LEFT JOIN meetings ON meetings.id = meeting_participant.meeting_id
+                LEFT JOIN users ON users.id = meetings.created_by
             `;
 
-            const record: any = await query(sql_record, meeting.id);
-            const participant: any = await query(sql_participant, meeting.id);
+        const params_meet: any[] = [];
+        const conditions_meet: string[] = [];
 
-            meeting.participant = participant;
-            meeting.record = record;
+        if(user_id) {
+            conditions.push('meeting_participant.user_id = ?');
+            params.push(user_id);
+        }
+
+        if (conditions_meet.length > 0) {
+            meet_participant += ' WHERE ' + conditions.join(' AND ');
+        }
+
+        const data: any = await query(meet_participant, params_meet);
+
+        const allMeetingsMap = new Map();
+
+        // Check apakah ada ?
+        // Kalo gaada kita ngambil lewat meeting participant
+        for (const meet of rows) {
+            allMeetingsMap.set(meet.id, meet);
+        }
+        
+        for (const meet of data) {
+            allMeetingsMap.set(meet.id, meet);
+        }
+        
+        const allMeetings = Array.from(allMeetingsMap.values());
+
+        let meeting = allMeetings;
+
+        if (meeting_code && meeting.length > 0) {
+            const selectedMeeting = meeting.find(m => m.meeting_code === meeting_code);
+
+            if (selectedMeeting) {
+                const sql_record = `
+                    SELECT id, record_name, created_at 
+                    FROM meeting_record 
+                    WHERE meeting_id = ?
+                `;
+                const sql_participant = `
+                    SELECT meeting_participant.id, users.fullname, users.email, status_accept.name AS status_name  
+                    FROM meeting_participant 
+                    LEFT JOIN users ON users.id = meeting_participant.user_id
+                    LEFT JOIN status_accept ON status_accept.id = meeting_participant.status_accept
+                    WHERE meeting_id = ?
+                `;
+
+                const record: any = await query(sql_record, [selectedMeeting.id]);
+                const participant: any = await query(sql_participant, [selectedMeeting.id]);
+
+                // Tambahkan ke object selectedMeeting
+                selectedMeeting.participant = participant;
+                selectedMeeting.record = record;
+            }
         }
 
         return {
             status: 200,
             message: 'SUCCESS_GET_MEETINGS',
-            data: rows
+            length: meeting.length,
+            data: meeting
         };
     }
 }
